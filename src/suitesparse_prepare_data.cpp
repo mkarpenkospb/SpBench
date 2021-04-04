11653c64fbc8abed6def6fa64787e6d7243c9322
Maria Karpenko <mkarpenko.spb@mail.ru>
2021-04-05 00:39:23 +0300

    [to server] hold error

--- src/suitesparse_prepare_data.cpp
+++ src/suitesparse_prepare_data.cpp
@@ -43,91 +43,100 @@ int main(int argc, const char **argv) {
 
     GrB_Matrix matrix = nullptr;
     GrB_Matrix result = nullptr;
-
+    bool *X = nullptr;
     for (auto &entry: argsProcessor.getEntries()) {
-        const auto &file = entry.name;
-        const auto &type = entry.isUndirected;
+        try {
+            const auto &file = entry.name;
+            const auto &type = entry.isUndirected;
 
-        MatrixLoader loader(file, type);
-        loader.loadData();
-        input = std::move(loader.getMatrix());
+            MatrixLoader loader(file, type);
+            loader.loadData();
+            input = std::move(loader.getMatrix());
 
-        size_t n = input.nrows;
-        assert(input.nrows == input.ncols);
+            size_t n = input.nrows;
+            assert(input.nrows == input.ncols);
 
-        std::cout << ">   Load matrix: \"" << file << "\" isUndirected: " << type << std::endl
-            << "                 size: " << input.nrows << " x " << input.ncols << " nvals: " << input.nvals
-            << std::endl;
+            std::cout << ">   Load matrix: \"" << file << "\" isUndirected: " << type << std::endl
+                      << "                 size: " << input.nrows << " x " << input.ncols << " nvals: " << input.nvals
+                      << std::endl;
 
 
-        // --------------------------- init matrix -----------------------------------------
-        GrB_CHECK(GrB_Matrix_new(&matrix, GrB_BOOL, n, n));
+            // --------------------------- init matrix -----------------------------------------
+            GrB_CHECK(GrB_Matrix_new(&matrix, GrB_BOOL, n, n));
 
-        std::vector<GrB_Index> I(input.nvals);
-        std::vector<GrB_Index> J(input.nvals);
+            std::vector<GrB_Index> I(input.nvals);
+            std::vector<GrB_Index> J(input.nvals);
 
-        bool *X = (bool *) std::malloc(sizeof(bool) * input.nvals);
+            X = (bool *) std::malloc(sizeof(bool) * input.nvals);
 
-        for (auto i = 0; i < input.nvals; i++) {
-            I[i] = input.rows[i];
-            J[i] = input.cols[i];
-            X[i] = true;
+            for (auto i = 0; i < input.nvals; i++) {
+                I[i] = input.rows[i];
+                J[i] = input.cols[i];
+                X[i] = true;
+            }
+
+            GrB_CHECK(GrB_Matrix_build_BOOL(matrix, I.data(), J.data(), X, input.nvals, GrB_FIRST_BOOL));
+
+            std::free(X);
+            X = nullptr;
+
+            // --------------------------- init result -----------------------------------------
+
+            GrB_CHECK(GrB_Matrix_new(&result, GrB_BOOL, n, n));
+
+            // -------------------------- execute multiplication ----------------------------------
+            GrB_CHECK(GrB_mxm(result, nullptr, nullptr, GrB_LOR_LAND_SEMIRING_BOOL, matrix, matrix, nullptr));
+
+            // -------------------------- read matrix --------------------------------------------
+            GrB_Index nvals;
+            GrB_Index nrows;
+            GrB_Index ncols;
+
+            GrB_CHECK(GrB_Matrix_nrows(&nrows, result));
+            GrB_CHECK(GrB_Matrix_ncols(&ncols, result));
+            GrB_CHECK(GrB_Matrix_nvals(&nvals, result));
+
+            std::cout << "Result matrix " << file << "2 : size: " << nrows << " x " << ncols << " nvals: " << nvals
+                      << std::endl;
+
+            Matrix m2;
+            m2.nrows = nrows;
+            m2.ncols = ncols;
+            m2.nvals = nvals;
+            m2.rows.resize(nvals);
+            m2.cols.resize(nvals);
+            I.resize(nvals);
+            J.resize(nvals);
+
+            std::cout << "Memory successfully allocated " << std::endl;
+
+            GrB_CHECK(GrB_Matrix_extractTuples_UINT32(I.data(), J.data(), nullptr, &nvals, result));
+
+            for (auto i = 0; i < nvals; i++) {
+                m2.rows[i] = I[i];
+                m2.cols[i] = J[i];
+                X[i] = true;
+            }
+
+
+            m2.nvals = nvals;
+
+            MatrixWriter writer;
+            writer.save(file + "2", m2);
+
+
+            GrB_CHECK(GrB_Matrix_free(&result));
+            GrB_CHECK(GrB_Matrix_free(&matrix));
+
+            result = nullptr;
+            matrix = nullptr;
+        } catch (const std::exception& e) {
+            std::cerr << "Exception caught : " << e.what() << std::endl;
+            if (X != nullptr) std::free(X);
+            if (result != nullptr) GrB_CHECK(GrB_Matrix_free(&result));
+            if (matrix != nullptr) GrB_CHECK(GrB_Matrix_free(&matrix));
+            throw std::exception(e);
         }
-
-        GrB_CHECK(GrB_Matrix_build_BOOL(matrix, I.data(), J.data(), X, input.nvals, GrB_FIRST_BOOL));
-
-        std::free(X);
-
-        // --------------------------- init result -----------------------------------------
-
-        GrB_CHECK(GrB_Matrix_new(&result, GrB_BOOL, n, n));
-
-        // -------------------------- execute multiplication ----------------------------------
-        GrB_CHECK(GrB_mxm(result, nullptr, nullptr, GrB_LOR_LAND_SEMIRING_BOOL, matrix, matrix, nullptr));
-
-        // -------------------------- read matrix --------------------------------------------
-        GrB_Index nvals;
-        GrB_Index nrows;
-        GrB_Index ncols;
-
-        GrB_CHECK(GrB_Matrix_nrows(&nrows, result));
-        GrB_CHECK(GrB_Matrix_ncols(&ncols, result));
-        GrB_CHECK(GrB_Matrix_nvals(&nvals, result));
-
-        std::cout << "Result matrix " << file << "2 : size: " << nrows << " x " << ncols << " nvals: " << nvals
-                  << std::endl;
-
-        Matrix m2;
-        m2.nrows = nrows;
-        m2.ncols = ncols;
-        m2.nvals = nvals;
-        m2.rows.resize(nvals);
-        m2.cols.resize(nvals);
-        I.resize(nvals);
-        J.resize(nvals);
-
-        std::cout << "Memory successfully allocated " << std::endl;
-
-        GrB_CHECK(GrB_Matrix_extractTuples_UINT32(I.data(), J.data(), nullptr, &nvals, result));
-
-        for (auto i = 0; i < input.nvals; i++) {
-            m2.rows[i] = I[i];
-            m2.cols[i] = J[i];
-            X[i] = true;
-        }
-
-
-        m2.nvals = nvals;
-
-        MatrixWriter writer;
-        writer.save(file + "2", m2);
-
-
-        GrB_CHECK(GrB_Matrix_free(&result));
-        GrB_CHECK(GrB_Matrix_free(&matrix));
-
-        result = nullptr;
-        matrix = nullptr;
     }
 
     return 0;
